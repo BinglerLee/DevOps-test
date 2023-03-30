d7a516fb2176fda1d320a2aa423468a74f966874
Bingler_Lee <lee.bingler@epitech.eu>
2023-03-30 17:48:31 +0200

    feat: try to figure why the checker always return 1

--- error.sh
+++ error.sh
@@ -6,26 +6,10 @@
 ## aezr
 ##
 
-input='coding-style-reports.log'
-IFS=':'
-i=0
-
-while read -r line; do
-    if [ $i == 0 ]; then
-        firstLine="$line"
-        printf "first: $firstLine\n\n"
-    fi
-    read -a strarr <<< "$line"
-
-    if [[ $i != 0 ]] && [[ $firstLine == $line ]]; then
-        printf "break\n"
-        break
-    fi
-
-    strarr[0]=$(echo "${strarr[0]}" | sed -r 's/^.{2}//')
-    strarr[2]=$(echo "${strarr[2]}" | sed -r 's/^.{1}//')
-    echo "+"
-    echo "::error file=${strarr[0]},line=${strarr[1]},endLine=$((${strarr[1]} + 1)),title="${strarr[2]} coding style error"::${strarr[3]}"
-    i=$((i+1))
-done < "$input"
-echo "-"
+if [ ! -f "coding-style-reports.log" ]; then
+    echo "oui"
+    exit 0
+elif [ $(wc -c "coding-style-reports.log" | awk '{print $1}') === "0" ]; then
+    exit 0
+fi
+exit 1
