4e680bcbfef72414dfb2ad89a6bba91c9f9a1c60
Bingler_Lee <lee.bingler@epitech.eu>
2023-03-30 16:44:41 +0200

    feat: try something

--- error.sh
+++ error.sh
@@ -9,10 +9,10 @@
 input='coding-style-reports.log'
 IFS=':'
 while read line; do
-    i=0
     read -a strarr <<< "$line"
     strarr[0]=$(echo "${strarr[0]}" | sed -r 's/^.{2}//')
     strarr[2]=$(echo "${strarr[2]}" | sed -r 's/^.{1}//')
-    echo "::error file=${strarr[0]},line=${strarr[1]},endline=$((${strarr[1]} + 1)),title="${strarr[2]} coding style error"::${strarr[3]}"
-done < $input
-exit 1
+    echo "+"
+    echo "::error file=${strarr[0]},line=${strarr[1]},endLine=$((${strarr[1]} + 1)),title="${strarr[2]} coding style error"::${strarr[3]}"
+done < "$input"
+echo "-"
