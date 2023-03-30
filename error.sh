4212f10129be24cfbd9566ca97883162854b2344
Bingler_Lee <lee.bingler@epitech.eu>
2023-03-30 13:55:01 +0200

    feat: try make error on github

---
+++ error.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+##
+## EPITECH PROJECT, 2023
+## error .sh
+## File description:
+## make an error in github
+##
+
+input="./coding-style-reports.log"
+
---
+++ main.c
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2023
+** maezr
+** File description:
+** maezr
+*/
+
+#include <stdio.h>
+
+int main(void) {
+    printf("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
+}
