ba8155a8b1634703530e96d07a98f42ef5e66b43
Bingler_Lee <lee.bingler@epitech.eu>
2023-03-30 17:05:45 +0200

    feat: try something in order to counter the double display

--- error.sh
+++ error.sh
@@ -8,11 +8,24 @@
 
 input='coding-style-reports.log'
 IFS=':'
+i=0
+
 while read -r line; do
+    if [ $i == 0 ]; then
+        firstLine="$line"
+        printf "$firstLine\n\n"
+    fi
     read -a strarr <<< "$line"
+
+    if [[ $i != 0 ]] && [[ $firstLine == $line ]]; then
+        printf "$i, $firstLine, $line\n"
+        break
+    fi
+
     strarr[0]=$(echo "${strarr[0]}" | sed -r 's/^.{2}//')
     strarr[2]=$(echo "${strarr[2]}" | sed -r 's/^.{1}//')
     echo "+"
     echo "::error file=${strarr[0]},line=${strarr[1]},endLine=$((${strarr[1]} + 1)),title="${strarr[2]} coding style error"::${strarr[3]}"
+    i=$((i+1))
 done < "$input"
 echo "-"
