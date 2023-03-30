0af1721248141481f500e6d595686f5380bc9e13
Bingler_Lee <lee.bingler@epitech.eu>
2023-03-30 15:58:10 +0200

    feat: display error coding style

--- error.sh
+++ error.sh
@@ -6,5 +6,14 @@
 ## make an error in github
 ##
 
-input="./coding-style-reports.log"
+input='coding-style-reports.log'
+IFS=':'
 
+while read line; do
+    i=0
+    echo "$line"
+    read -a strarr <<< "$line"
+    strarr[0]=$(echo "${strarr[0]}" | sed -r 's/^.{2}//')
+    strarr[2]=$(echo "${strarr[2]}" | sed -r 's/^.{1}//')
+    echo "::error file=${strarr[0]},line=${strarr[1]},title=${strarr[2]} ::${strarr[3]}"
+done < $input
