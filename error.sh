141f4073642d7132b7ce0f3b2c9c48e582326c9b
Bingler_Lee <lee.bingler@epitech.eu>
2023-03-30 17:52:05 +0200

    feat: add aezraraez

--- error.sh
+++ error.sh
@@ -9,7 +9,9 @@
 if [ ! -f "coding-style-reports.log" ]; then
     echo "oui"
     exit 0
-elif [ $(wc -c "coding-style-reports.log" | awk '{print $1}') === "0" ]; then
+elif [ $(wc -c 'coding-style-reports.log' | awk '{print $1}') == "0" ]; then
+    echo "non"
     exit 0
 fi
+echo "un"
 exit 1
